8fc7d8f161ed4c024ae2dbfd4e24c16f93c487c9
ygh0191 <ygh0191@gmail.com>
2019-12-02 04:13:28 +0900

    line 추가

--- src/newTrial.java
+++ src/newTrial.java
@@ -2,5 +2,8 @@
 public class newTrial {
 	public static void main(String args[]) {
 		System.out.println("My first java Trial");
+		System.out.println("My first java Trial");
+		System.out.println("My first java Trial");
+		
 	}
 }
